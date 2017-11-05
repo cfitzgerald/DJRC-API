a987a2799e38112ef01581f4d3d0aaae422c1909
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-11-05 14:55:08 -0500

    add basic store

--- client/index.js
+++ client/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
+import store from './store';
 import Main from './components/Main';
 
 ReactDOM.render(
-  <Router>
-    <Main />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Main />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
