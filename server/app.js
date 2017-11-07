f2e0095cd5714cf6c1290cd2fa328bf50a7b1467
Jonathan Brandwein <jonathan.brandwein@gmail.com>
2017-11-06 22:45:59 -0500

    merged new seed

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const db = require('./db/models/').db;
+const sync = require('./db/models/').sync;
 const port = 3002;
 
 const bodyParser = require('body-parser');
@@ -23,7 +23,7 @@ app.use((err, req, res, next) => {
     res.send(err);
 });
 
-db.sync()
+sync()
   .then(() => {
       console.log('synced');
       app.listen(port, () => {
