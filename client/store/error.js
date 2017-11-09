d2c651c74b492c63a4af16bc8cc751306ba788e6
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-11-08 22:06:07 -0500

    add auth to store; update user store with comments

--- client/store/error.js
+++ client/store/error.js
@@ -1,9 +1,12 @@
+// ACTION TYPE(s)
 const SET_ERROR = 'SET_ERROR';
 
+// ACTION CREATOR(s)
 export function setError(msg){
   return {type: SET_ERROR, msg};
 }
 
+// REDUCER(s)
 export default function reducer(state = '', action){
   switch (action.type){
     case SET_ERROR:
