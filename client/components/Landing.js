0a7adeebc53f3635dc66567ca66635de1da3293f
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-11-23 18:15:16 -0500

    Landing now a dumb component

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,116 +1,88 @@
-import React, { Component } from 'react';
-// import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from 'react';
 
-// components
-// import SignupForm from './SignupForm';
+const Landing = () => {
 
-// store
-// import { fetchVenueCount } from '../store';
+  return (
+    <div>
 
-class Landing extends Component {
+      <div id="landing-header" className="container-fluid text-center">
 
-  componentDidMount() {
-    // this.props.fetchVenueCount();
-  }
-
-  render() {
-
-    // const { venues } = this.props;
-
-    return (
-      <div>
-
-        <div id="landing-header" className="container-fluid text-center">
-
-          <div className="col-sm-12">
-            {/* <img className="img-fluid" src="/public/images/barcast-logo.png" alt="BarCast logo" /> */}
-            <h1 id="landing-title">BarCast</h1>
-            <p id="landing-lead">A curated suggestion engine to find local bars playing the genre of music you want to hear right now.</p>
-          </div>
-
-          <hr className="landing-hr-yellow" />
-          <hr className="landing-hr-pink" />
-          <hr className="landing-hr-blue" />
-
+        <div className="col-sm-12">
+          <h1 id="landing-title">BarCast</h1>
+          <p id="landing-lead">A curated suggestion engine to find local bars playing the genre of music you want to hear right now.</p>
         </div>
 
-        <div id="landing-body" className="container">
+        <hr className="landing-hr-yellow" />
+        <hr className="landing-hr-pink" />
+        <hr className="landing-hr-blue" />
 
-          <div id="features" className="container">
-            <div className="row">
+      </div>
 
-              <div className="col-sm-4">
-                <h4>Why use it...</h4>
-              </div>
+      <div id="landing-body" className="container">
 
-              <div className="col-sm-8">
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item">We've got so many venues!</li>
-                  <li className="list-group-item">For users...</li>
-                  <li className="list-group-item">For venue owners...</li>
-                </ul>
-              </div>
+        <div id="features" className="container">
+          <div className="row">
 
+            <div className="col-sm-4">
+              <h4>Why use it...</h4>
             </div>
-          </div>
 
-          <hr className="landing-hr-gray" />
-
-          <div id="tech" className="container">
-            <div className="row">
-
-              <div className="col-sm-4">
-                <h4>Technologies/APIs</h4>
-              </div>
-
-              <div className="col-sm-8">
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item">React Native</li>
-                  <li className="list-group-item">Google Maps API</li>
-                  <li className="list-group-item">Spotify API</li>
-                  <li className="list-group-item">Maybe Gracenote API</li>
-                  <li className="list-group-item">Maybe Echo Nest API</li>
-                  <li className="list-group-item">...</li>
-                </ul>
-              </div>
-
+            <div className="col-sm-8">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">We've got so many venues!</li>
+                <li className="list-group-item">For users...</li>
+                <li className="list-group-item">For venue owners...</li>
+              </ul>
             </div>
-          </div>
 
+          </div>
         </div>
 
-        <div id="landing-footer" className="container-fluid text-center">
+        <hr className="landing-hr-gray" />
 
-          <hr className="landing-hr-yellow" />
-          <hr className="landing-hr-pink" />
-          <hr className="landing-hr-blue" />
+        <div id="tech" className="container">
+          <div className="row">
 
-          <div id="team" className="container">
+            <div className="col-sm-4">
+              <h4>Technologies/APIs</h4>
+            </div>
 
-            <ul className="list-group list-group-flush">
-              <li><strong>D</strong>an Weissbard</li>
-              <li><strong>J</strong>onathan Brandwein</li>
-              <li><strong>R</strong>avish Rawal</li>
-              <li><strong>C</strong>olin FitzGerald</li>
-            </ul>
+            <div className="col-sm-8">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">React Native</li>
+                <li className="list-group-item">Google Maps API</li>
+                <li className="list-group-item">Spotify API</li>
+                <li className="list-group-item">Maybe Gracenote API</li>
+                <li className="list-group-item">Maybe Echo Nest API</li>
+                <li className="list-group-item">...</li>
+              </ul>
+            </div>
 
           </div>
         </div>
 
       </div>
-    );
-  }
-}
 
-const mapStateToProps = () => {
-  return {
-  };
+      <div id="landing-footer" className="container-fluid text-center">
+
+        <hr className="landing-hr-yellow" />
+        <hr className="landing-hr-pink" />
+        <hr className="landing-hr-blue" />
+
+        <div id="team" className="container">
+
+          <ul className="list-group list-group-flush">
+            <li><strong>D</strong>an Weissbard</li>
+            <li><strong>J</strong>onathan Brandwein</li>
+            <li><strong>R</strong>avish Rawal</li>
+            <li><strong>C</strong>olin FitzGerald</li>
+          </ul>
+
+        </div>
+      </div>
+
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Landing);
+export default Landing;
