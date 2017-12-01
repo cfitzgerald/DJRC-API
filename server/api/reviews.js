eff01e94c53da40e413e573a0346c1c5a4d52e92
Jonathan Brandwein <jonathan.brandwein@gmail.com>
2017-11-30 20:33:27 -0500

    commit

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const Review = require('../db/models/Review');
+const db = require('../db/models/');
+const { Venue, Review } = db;
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -11,8 +12,27 @@ router.get('/', (req, res, next) => {
 
 router.post('/:barId', (req, res, next) => {
     Review.create(req.body)
-        .then(reviews => {
-            res.send(reviews);
+        .then(review => {
+            return Promise.all([Review.findAll({
+                where: {
+                    venueId: req.params.barId
+                }
+            }),
+            Venue.findById(req.params.barId)]
+            )
+                .then(([reviews, venue]) => {
+                    let avg = 0;
+                    reviews.forEach(review => {
+                        avg += Number(review.rating);
+                    })
+                    avg = avg / reviews.length;
+                    venue.avgRating = avg;
+                    venue.save()
+                        .then(() => {
+                            res.send(review);
+                        })
+
+                })
         })
 })
 
