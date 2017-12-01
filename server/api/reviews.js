b867e590d992b1a3db91263783c70b2e6b1b0e88
Jonathan Brandwein <jonathan.brandwein@gmail.com>
2017-12-01 07:51:14 -0500

    av

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -14,6 +14,7 @@ router.post('/:barId', (req, res, next) => {
     console.log('bar')
     Review.create(req.body)
         .then(review => {
+            console.log('ereaer', review);
             return Promise.all([Review.findAll({
                 where: {
                     venueId: req.params.barId
