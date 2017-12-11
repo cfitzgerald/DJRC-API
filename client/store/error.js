51c278c300f9cde3d3cc959879ab0b0c2675da10
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-12-10 20:56:31 -0500

    linting and formatting for client/store

--- client/store/error.js
+++ client/store/error.js
@@ -2,16 +2,19 @@
 const SET_ERROR = 'SET_ERROR';
 
 // ACTION CREATOR(s)
-export function setError(msg){
-  return {type: SET_ERROR, msg};
+export function setError(msg) {
+    return {
+        type: SET_ERROR,
+        msg
+    };
 }
 
 // REDUCER(s)
-export default function reducer(state = '', action){
-  switch (action.type){
-    case SET_ERROR:
-      return action.msg;
-    default:
-      return state;
-  }
+export default function reducer(state = '', action) {
+    switch (action.type) {
+        case SET_ERROR:
+            return action.msg;
+        default:
+            return state;
+    }
 }
