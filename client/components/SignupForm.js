4ad4ef361fa4b6987fb6779eed81a9bb01bec03e
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-12-10 21:17:36 -0500

    linting and formatting for client/components

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -2,124 +2,110 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SignupForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            firstName: '',
+            lastName: '',
+            password: '',
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        const change = {};
+        change[event.target.name] = event.target.value;
+        this.setState(change);
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        const { email, firstName, lastName, password } = this.state;
+        this.setState({ email: '', firstName: '', lastName: '', password: '' });
+    }
+    componentDidMount() {
+        // console.log("componentDidMount: SignupForm");
+    }
+    render() {
+        const { handleSubmit, handleChange } = this;
+        const { email, firstName, lastName, password } = this.state;
 
-  constructor() {
-    super();
-    this.state = {
-      email: '',
-      firstName: '',
-      lastName: '',
-      password: '',
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event){
-    const change = {};
-    change[event.target.name] = event.target.value;
-    this.setState(change);
-  }
-
-  handleSubmit(event){
-    event.preventDefault();
-    const { email, firstName, lastName, password } = this.state;
-    this.setState({ email: '', firstName: '', lastName: '', password: '' });
-  }
-
-  componentDidMount(){
-    // console.log("componentDidMount: SignupForm");
-  }
-
-  render() {
-
-    const { handleSubmit, handleChange } = this;
-    const { email, firstName, lastName, password } = this.state;
-
-    return (
-      <div className="row">
-        <div id="signup-form-container" className="col-md-12">
-
-          <p id="signup-lead" className="text-center">Create a free account!</p>
-
-          <form id="signup-form" className="card" onSubmit={handleSubmit}>
-
-            <div className="form-row">
-              <div className="form-group col-md-6">
-                <div className="form-group">
-                  <input
-                    className="form-control"
-                    name="firstName"
-                    onChange={handleChange}
-                    placeholder="First name..."
-                    type="text"
-                    value={firstName} />
+        return (
+            <div className="row">
+                <div id="signup-form-container" className="col-md-12">
+                    <p id="signup-lead" className="text-center">Create a free account!</p>
+                    <form id="signup-form" className="card" onSubmit={handleSubmit}>
+                        <div className="form-row">
+                            <div className="form-group col-md-6">
+                                <div className="form-group">
+                                    <input
+                                        className="form-control"
+                                        name="firstName"
+                                        onChange={handleChange}
+                                        placeholder="First name..."
+                                        type="text"
+                                        value={firstName} />
+                                </div>
+                            </div>
+                            <div className="form-group col-md-6">
+                                <div className="form-group">
+                                    <input
+                                        className="form-control"
+                                        name="lastName"
+                                        onChange={handleChange}
+                                        placeholder="Last name..."
+                                        type="text"
+                                        value={lastName} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="form-row">
+                            <div className="form-group col-md-6">
+                                <div className="form-group">
+                                    <input
+                                        className="form-control"
+                                        name="email"
+                                        onChange={handleChange}
+                                        placeholder="Email address..."
+                                        type="text"
+                                        value={email} />
+                                </div>
+                            </div>
+                            <div className="form-group col-md-6">
+                                <div className="form-group">
+                                    <input
+                                        className="form-control"
+                                        name="password"
+                                        onChange={handleChange}
+                                        placeholder="Set a password..."
+                                        type="password"
+                                        value={password} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="form-row">
+                            <div className="form-group col-md-12 text-center">
+                                <button
+                                    className="btn btn-outline-primary btn-md text-center"
+                                    id="signup-button"
+                                    type="submit">Create account</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-              </div>
-
-              <div className="form-group col-md-6">
-                <div className="form-group">
-                  <input
-                    className="form-control"
-                    name="lastName"
-                    onChange={handleChange}
-                    placeholder="Last name..."
-                    type="text"
-                    value={lastName} />
-                </div>
-              </div>
             </div>
-
-            <div className="form-row">
-              <div className="form-group col-md-6">
-                <div className="form-group">
-                  <input
-                    className="form-control"
-                    name="email"
-                    onChange={handleChange}
-                    placeholder="Email address..."
-                    type="text"
-                    value={email} />
-                </div>
-              </div>
-
-              <div className="form-group col-md-6">
-                <div className="form-group">
-                  <input
-                    className="form-control"
-                    name="password"
-                    onChange={handleChange}
-                    placeholder="Set a password..."
-                    type="password"
-                    value={password} />
-                </div>
-              </div>
-            </div>
-
-            <div className="form-row">
-              <div className="form-group col-md-12 text-center">
-                <button
-                  className="btn btn-outline-primary btn-md text-center"
-                  id="signup-button"
-                  type="submit">Create account</button>
-              </div>
-            </div>
-
-          </form>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 const mapStateToProps = () => {
-  return {
-  };
+    return {
+    };
 };
 
 const mapDispatchToProps = () => {
-  return {
-  };
+    return {
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignupForm);
--- client/components/WebAdmin.js
+++ client/components/WebAdmin.js
@@ -3,103 +3,82 @@ import { connect } from 'react-redux';
 import { fetchVenues, fetchUsers, updateOwner, addPromo } from '../store';
 
 class WebAdmin extends Component {
-  constructor(){
-    super();
-    this.state = {
-      filterValue: '',
-      userId: 0
-
+    constructor() {
+        super();
+        this.state = {
+            filterValue: '',
+            userId: 0
+        };
+        this.handleFilter = this.handleFilter.bind(this);
+        this.onChangeHandler = this.onChangeHandler.bind(this);
     }
-    this.handleFilter = this.handleFilter.bind(this);
-
-    this.onChangeHandler = this.onChangeHandler.bind(this)
-  }
-
-  componentDidMount(){
-    this.props.fetchVenues();
-    this.props.fetchUsers();
-  }
-  handleFilter(ev){
-    this.setState({filterValue: ev.target.value})
-  }
-
-  onChangeHandler(ev){
-    this.setState({userId: ev.target.value * 1})
-  }
-
-
-
-  render(){
-    const { handleFilter, onChangeHandler } = this;
-    const { filterValue } = this.state;
-    let filteredVenues, users
-    if (this.props.venues){
-    filteredVenues = this.props.venues.filter(venue => venue.name.match(filterValue));
+    componentDidMount() {
+        this.props.fetchVenues();
+        this.props.fetchUsers();
     }
-
-    if (this.props.users){
-      users = this.props.users.filter(user => user.isBusiness !== true)
+    handleFilter(ev) {
+        this.setState({ filterValue: ev.target.value });
     }
+    onChangeHandler(ev) {
+        this.setState({ userId: ev.target.value * 1 });
+    }
+    render() {
+        const { handleFilter, onChangeHandler } = this;
+        const { filterValue } = this.state;
+        let filteredVenues, users;
+        if (this.props.venues) {
+            filteredVenues = this.props.venues.filter(venue => venue.name.match(filterValue));
+        }
+        if (this.props.users) {
+            users = this.props.users.filter(user => user.isBusiness !== true);
+        }
 
-    return(
-     <div>
-      <h2>Bar Name Filter</h2>
-       <form className='form-group'>
-        <input
-          onChange={ handleFilter }
-          value={ filterValue }
-          className='form-control'
-         />
-       </form>
-       <table className="table">
-        <thead>
-          <tr>
-            <th> ID </th>
-            <th> Venue Name </th>
-          </tr>
-        </thead>
-        <tbody>
-
-
-
-          {
-            filteredVenues.map( bar => {
-              return (
-                <tr key={ bar.id }>
-                  <td> { bar.id } </td>
-                  <td> { bar.name } </td>
-                  <td>
-                    <form onSubmit={()=> {
-
-                      this.props.updateOwner(bar.id, this.state.userId)}}
-                      >
-                      <select onChange={ onChangeHandler }>
-
-
+        return (
+            <div>
+                <h2>Bar Name Filter</h2>
+                <form className="form-group">
+                    <input
+                        className="form-control"
+                        onChange={ handleFilter }
+                        value={ filterValue } />
+                </form>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th> ID </th>
+                            <th> Venue Name </th>
+                        </tr>
+                    </thead>
+                    <tbody>
                         {
-                          users.map( user => {
-                            return(
-                              <option key= {user.id} value={user.id}> {user.id} - {user.email} </option>
-                            )
-                          })
+                            filteredVenues.map(bar => {
+                                return (
+                                    <tr key={ bar.id }>
+                                        <td> { bar.id } </td>
+                                        <td> { bar.name } </td>
+                                        <td>
+                                            <form onSubmit={() => {this.props.updateOwner(bar.id, this.state.userId);}}>
+                                                <select onChange={ onChangeHandler }>
+                                                    {
+                                                        users.map(user => {
+                                                            return (
+                                                                <option key= {user.id} value={user.id}> {user.id} - {user.email} </option>
+                                                            );
+                                                        })
+                                                    }
+                                                </select>
+                                                <button className="btn btn-primary">Update</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                );
+                            })
                         }
-                      </select>
-                      <button className="btn btn-primary"> Update </button>
-                    </form>
-                  </td>
-                </tr>
-              )
-            })
-           }
-
-        </tbody>
-
-      </table>
-     </div>
-    )
-
-  }
-
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 const mapState = ({ venues, users }) => {
@@ -116,7 +95,7 @@ const mapDispatch = (dispatch) => {
         },
         updateOwner,
         addPromo
-    }
-}
+    };
+};
 
 export default connect(mapState, mapDispatch)(WebAdmin);
